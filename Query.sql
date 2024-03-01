c814427b05ff80fd9c05d2f7b73edbc715ae975b
PawanJadhav7 <pawan.jadhav7@gmail.com>
2024-02-29 21:30:52 -0500

    added sql queries 1-5

--- Query.sql
+++ Query.sql
@@ -23,8 +23,8 @@ SELECT  games, count(1) as total_countries
 FROM all_countries
 GROUP BY games
 ORDER BY games
---4. Which year saw the highest and lowest no of countries participating in olympics
 
+--4. Which year saw the highest and lowest no of countries participating in olympics
 WITH participating_countries as
 (
 	select games, region
@@ -39,9 +39,51 @@ tot_countries as
 	GROUP BY games
 	ORDER BY total_countries
 )
-SELECT games||'-'|| MIN(total_countries) as lowest_countries 
+SELECT DISTINCT
+	concat(first_value(games) over(order by total_countries), '-', 
+		  	first_value(total_countries) over(order by total_countries)) as Lowest_Countries,
+			
+	concat(first_value(games) over(order by total_countries DESC), '-',
+		  	first_value(total_countries) over(order by total_countries DESC)) as Highest_Countries
 FROM tot_countries
-GROUP BY games
+
+--5. Which nation has participated in all of the olympic games
+
+WITH tot_games as
+(
+	SELECT COUNT(DISTINCT(games)) as total_games
+	FROM OLYMPICS_HISTORY
+),
+countries as
+(
+	SELECT  games, nr.region as country
+    FROM olympics_history oh
+    JOIN olympics_history_noc_regions nr ON nr.noc=oh.noc
+    GROUP BY games, nr.region 
+),
+countries_participated as
+(
+	SELECT country, count(1) as total_participated_games
+	FROM countries
+	GROUP BY country
+)
+SELECT 
+		cp.*
+FROM
+		countries_participated cp
+JOIN
+		tot_games tg on tg.total_games = cp.total_participated_games
+ORDER BY 
+		1
+
+
+
+
+
+
+
+
+
 
 
 
